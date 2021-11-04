981a2e62a2a11043c9d76f229c5e02803cdf6813
abtonkar <antonk@default-value.com>
2021-11-05 00:10:22 +0200

    #3 MVC: Added additional CMS page

--- src/antonkar/Cms/Controller/Page.php
+++ src/antonkar/Cms/Controller/Page.php
@@ -8,10 +8,20 @@ use antonkar\Framework\Http\ControllerInterface;
 
 class Page implements ControllerInterface
 {
+    private \antonkar\Framework\Http\Request $request;
+
+    /**
+     * @param \antonkar\Framework\Http\Request $request
+     */
+    public function __construct(
+        \antonkar\Framework\Http\Request $request
+    ) {
+        $this->request = $request;
+    }
 
     public function execute(): string
     {
-        $page = 'home.php';
+        $page = $this->request->getParameter('page') . '.php';
 
         ob_start();
         require_once "../src/page.php";
--- src/antonkar/Cms/Router.php
+++ src/antonkar/Cms/Router.php
@@ -8,17 +8,32 @@ use antonkar\Cms\Controller\Page;
 
 class Router implements \antonkar\Framework\Http\RouterInterface
 {
+    private \antonkar\Framework\Http\Request $request;
+
+    /**
+     * @param \antonkar\Framework\Http\Request $request
+     */
+    public function __construct(
+        \antonkar\Framework\Http\Request $request
+    ) {
+        $this->request = $request;
+    }
+
     /**
      * @inheritDoc
      */
     public function match(string $requestUrl): string
     {
-        if ($requestUrl == '')
-        {
+        $cmsPage = [
+            '',
+            'test-demo-page'
+        ];
+
+        if (in_array($requestUrl, $cmsPage)) {
+            $this->request->setParameter('page', $requestUrl ?: 'home');
             return Page::class;
         }
 
         return '';
-
     }
 }
